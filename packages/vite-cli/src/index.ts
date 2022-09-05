ae37a3ec234c6f2d124efeefe8bd379f902020ba
yejf <490293266@qq.com>
2022-09-05 20:12:56 +0800

    update

--- packages/vite-cli/src/index.ts
+++ packages/vite-cli/src/index.ts
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
-import { Command } from "commander"
-// const Chalk = require("chalk");
+import program from "./core/program"
+// import viteCliCoreCommand from "./core/command";
 
-const program = new Command()
+// program.version("1.0.0", "-V, --version").usage("<command> [options]")
 
-program.version("1.0.0", "-V, --version").usage("<command> [options]")
+// program
+//   .command("create <app-name>")
+//   .description("create a new project powered by project-cli-service")
+//   .option("-d, --default", "Skip prompts and use default preset")
+//   .action(name => {
+//     console.log(chalk.blue(`${name}项目正在创建中...`))
+//   })
 
-program
-  .command("create <app-name>")
-  .description("create a new project powered by project-cli-service")
-  .option("-d, --default", "Skip prompts and use default preset")
-  .action((name: any) => {
-    // console.log(chalk.blue(`${name}项目正在创建中...`))
-    console.log(`${name}项目正在创建中...`)
-  })
+// program.parse(process.argv)
 
-program.parse(process.argv)
+async function createViteCliCommand() {
+  // await viteCliCoreCommand()
+  program.parse(process.argv)
+}
+
+createViteCliCommand()
